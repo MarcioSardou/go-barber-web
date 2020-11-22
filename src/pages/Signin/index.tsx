9d1e1f49e679899be2b9edbf2265c28fd6a6f87a
Marcio Sardou <marciosut1996@gmail.com>
2020-11-22 17:38:52 -0300

    ajustes iniciais

---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+
+const SignIn: React.FC = () => <h1>Signin</h1>;
+export default SignIn;
---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding:0;
+    box-sizing:0;
+    outline:0;
+  }
+
+  body {
+    background: #312E38;
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
